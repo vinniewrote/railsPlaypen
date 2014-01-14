258fbb791a7fcb64ded23ef54029b5d738b32830
Vizente Freeman <vizente@sandboxstudio.net>
2014-01-14 12:01:40 -0600

    initial commit
    first commit of baby chart rails project

---
+++ babyChart/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BabyChart::Application.config.secret_key_base = '4af94dbdfd0491d87932a0d2cde7f6125ab6255abc2689164f8339d4b96f01b0a521763f3e3b326a36708ec81505df8dbef15b19709673be5eee63ec3be048d9'
